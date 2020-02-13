b6ef2d1aa3f5055343385a77b7cabbd1096314d7
Akash Chhetri <akash.chhetri1@gmail.com>
2020-02-13 12:20:18 +1100

    01- Refactored and added function

--- sentence_maker.py
+++ sentence_maker.py
@@ -1,5 +1,21 @@
-sentance = ''
-question = ("how","when","what","where","who","whom","why","which",'whose')
+# global variable of list sentence
+sentence = []
+
+# find out whether statement is a question
+def add_question_mark(phrase):
+    question = ("how","when","what","where","who","whom","why","which",'whose')
+    for q in question:
+        if phrase.find(q) != -1:
+            return "%s?" % (phrase)
+    return "%s." % (phrase)
+
+# take list and return report
+def list_to_report(phrase):
+    '''This function returns after joining list'''
+    return " ".join(phrase)
+
+
+# main script
 while(True):
     # input sentance from user
     say_something = input("Say something: ")
@@ -8,12 +24,7 @@ while(True):
     if say_something == '/end':
         break
     
-    # find out whether statement is a question
-    sign = '.'
-    for q in question:
-        if say_something.find(q) != -1:
-            sign = '?'
-    
-    sent = "%s%s%s " % (sentance,say_something.capitalize(),sign)
+    # adding to list after capitalize
+    sentence.append(add_question_mark(say_something).capitalize())
 
-print(sent)
+print(list_to_report(sentence))
